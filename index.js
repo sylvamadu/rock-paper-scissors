5273418c122175f9013e3c5d0ec9c73c005a28de
sylvamadu <54332482+sylvamadu@users.noreply.github.com>
2022-08-01 07:00:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import AppViews from './views/AppViews';
-import DeployerViews from './views/DeployerViews';
-import AttacherViews from './views/AttacherViews';
-import {renderDOM, renderView} from './views/render';
+import AppViews from './views/AppViews.js';
+import DeployerViews from './views/DeployerViews.js';
+import AttacherViews from './views/AttacherViews.js';
+import {renderDOM, renderView} from './views/render.js';
 import './index.css';
 import * as backend from './build/index.main.mjs';
 import { loadStdlib } from '@reach-sh/stdlib';
